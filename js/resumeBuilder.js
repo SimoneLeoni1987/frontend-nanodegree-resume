66fe55959e9ac82f7bd8d8ae84393a59f76cab6f
Simone <Simone@pc-jdk.it>
2015-10-23 17:14:23 +0200

    Aggiunto oggetto bio

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,22 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+var formattedName = HTMLheaderName.replace("%data%", "Simone");
+var formattedRole = HTMLheaderRole.replace("%data%", "Scimmia");
+
+$("#header").prepend(formattedName);
+$("#header").append(formattedRole);
+
+var skills = ["idiot", "banana-eater"];
+
+var bio = {
+    name : "Simone",
+    role : "Scimmia",
+    contactInfo : "No contact info",
+    picURL : "http://weknowyourdreams.com/images/monkey/monkey-05.jpg",
+    welcomeMessage : "Welcome monkeyss",
+    skills : skills
+};
+
+$("#bio")
+
