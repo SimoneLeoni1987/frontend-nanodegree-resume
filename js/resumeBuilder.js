cb599767b78a365d562250fae0fdbb19e6cfbf16
Simone <Simone@pc-jdk.it>
2015-10-23 17:47:34 +0200

    Aggiunta immagine messaggio di benvenuto

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,22 +1,41 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
-var formattedName = HTMLheaderName.replace("%data%", "Simone");
-var formattedRole = HTMLheaderRole.replace("%data%", "Scimmia");
+var bio = {
+    name : "Simone",
+    role : "Monkey",
+    contactInfo : {
+        skype : "Skype contact",
+        phone : "1666666"
+    },
+    picURL : "http://www.stock-monkey.com/images/bald-monkey.gif",
+    welcomeMessage : "Welcome monkeyss",
+    skills : ["idiot", "banana-eater"]
+};
+
+var work = {};
+work.currentPosition = "Banana eater senior";
+work.yearsWorked = 5;
+work.city = "Rome";
+
+var education = {};
+education["lastSchool"] = "Zoo";
+education["city"] = "Jungle";
+
+var project = [
+    {
+        name : "BananaBook",
+        year : 2000
+    },
+    {
+        name : "Twibber",
+        year : 2005
+    }
+];
+
+var formattedName = HTMLheaderName.replace("%data%", bio.name);
+var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
+var formattedWelcomeMessage = HTMLwelcomeMsg.replace("%data%", bio.welcomeMessage);
+var formattedBioPic = HTMLbioPic.replace("%data%", bio.picURL);
 
 $("#header").prepend(formattedName);
 $("#header").append(formattedRole);
-
-var skills = ["idiot", "banana-eater"];
-
-var bio = {
-    name : "Simone",
-    role : "Scimmia",
-    contactInfo : "No contact info",
-    picURL : "http://weknowyourdreams.com/images/monkey/monkey-05.jpg",
-    welcomeMessage : "Welcome monkeyss",
-    skills : skills
-};
-
-$("#bio")
-
+$("#header").append(formattedWelcomeMessage);
+$("#header").append(formattedBioPic);
